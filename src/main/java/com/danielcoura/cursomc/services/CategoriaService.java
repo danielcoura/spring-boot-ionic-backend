db80671ab480d8193fc91e6b028162dadcd951eb
Daniel Coura <danielcoura@gmail.com>
2020-07-06 19:44:15 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/danielcoura/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.danielcoura.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danielcoura.cursomc.domain.Categoria;
+import com.danielcoura.cursomc.repositories.CategoriaRepository;
+
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElse(null);
+	}
+}
