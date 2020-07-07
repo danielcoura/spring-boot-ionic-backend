fb240181d54bee7efd20c50f48fed8496239c011
Daniel Coura <danielcoura@gmail.com>
2020-07-06 21:54:39 -0300

    Criando associacoes Produto Categoria

--- src/main/java/com/danielcoura/cursomc/domain/Categoria.java
+++ src/main/java/com/danielcoura/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.danielcoura.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -17,6 +20,9 @@ public class Categoria implements Serializable{
 
 	private String nome;
 
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<Produto>();
+	
 	public Categoria() {
 		
 	}
@@ -68,4 +74,13 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	
 }
