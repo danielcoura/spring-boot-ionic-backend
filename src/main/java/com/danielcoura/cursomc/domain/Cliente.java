f88f8672f8c58cbfc7f6f2d8b886776edb844758
Daniel Coura <danielcoura@gmail.com>
2020-07-07 14:14:58 -0300

    Endpoint cliente id disponivel

--- src/main/java/com/danielcoura/cursomc/domain/Cliente.java
+++ src/main/java/com/danielcoura/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.danielcoura.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente implements Serializable{
@@ -33,6 +34,7 @@ public class Cliente implements Serializable{
 	
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<Endereco>();
 	
@@ -51,7 +53,6 @@ public class Cliente implements Serializable{
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = tipo.getCod();
-		this.enderecos = enderecos;
 	}
 
 	public Integer getId() {
